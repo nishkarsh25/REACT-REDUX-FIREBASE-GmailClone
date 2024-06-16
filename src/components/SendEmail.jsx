1ea8bcf2ee4367b7ad152d1a99620a308c350d5f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:54:03 +0530

    Import Statements React: Importing React, useState for managing component state. Icons: Importing RxCross2 from react-icons for a close button icon. Redux: Importing useDispatch, useSelector from react-redux to interact with the Redux store. Firebase: Importing db from the Firebase configuration, and collection, addDoc, serverTimestamp from firebase/firestore for handling Firestore database operations.

---
+++ src/components/SendEmail.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { RxCross2 } from "react-icons/rx";
+import { useDispatch, useSelector } from 'react-redux';
+import { setOpen } from '../redux/appSlice';
+import { db } from '../firebase';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+
+
+
