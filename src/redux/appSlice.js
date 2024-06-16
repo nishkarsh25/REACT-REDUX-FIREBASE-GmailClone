70b71876df3c7389dbbb706d14ccdb57e8cdfbdf
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:35:55 +0530

    c. reducers The reducers property is an object where each key is a reducer function that can modify the state. Reducers are responsible for handling actions and updating the state accordingly. Each reducer function receives the current state and an action as arguments.
    Reducer Functions:
    setOpen: This reducer updates the open property in the state. It sets the open property to the value provided in the action.payload.
    setSelectedMail: This reducer updates the selectedMail property with the value provided in the action.payload.
    setSearchText: This reducer sets the searchText property to the value provided in the action.payload.
    setEmails: This reducer updates the emails property with the value provided in the action.payload.
    setAuthUser: This reducer sets the authUser property to the value provided in the action.payload.

--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -9,7 +9,23 @@ const appSlice = createSlice({
         emails: null, // This might need to be initialized as an array or object depending on your application logic
         authUser: null
     },
-    
+    reducers: {
+        setOpen: (state, action) => {
+            state.open = action.payload;
+        },
+        setSelectedMail: (state, action) => {
+            state.selectedMail = action.payload;
+        },
+        setSearchText: (state, action) => {
+            state.searchText = action.payload;
+        },
+        setEmails: (state, action) => {
+             state.emails = action.payload;
+        },
+        setAuthUser: (state, action) => {
+            state.authUser = action.payload;
+        }
+    }
 });
 
 
