cd98b4c4a3a798203a7d78d60edbb07a95763726
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:51:26 +0530

    Import Statements React and State Management: React, useEffect, and useState from react. useDispatch and useSelector from react-redux. Icons: Various icons from react-icons (e.g., IoSettingsOutline, FaRegQuestionCircle, RxHamburgerMenu, etc.) for visual elements. Avatar: Avatar from react-avatar for displaying user profile images. Firebase: auth and signOut from firebase/auth for handling authentication. Animations: motion and AnimatePresence from framer-motion for adding animations.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { PiDotsNineBold } from "react-icons/pi";
+import { IoIosSearch } from "react-icons/io";
+import Avatar from 'react-avatar';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthUser, setSearchText } from '../redux/appSlice';
+import { auth } from '../firebase';
+import { signOut } from 'firebase/auth';
+import { motion, AnimatePresence } from 'framer-motion';
+
+
+
+
