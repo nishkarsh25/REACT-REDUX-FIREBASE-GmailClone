6ee1617fdd0ee43c8b0bff13810fa486274ad414
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 09:02:58 +0530

    Framer Motion Animation motion.div: Wraps the entire content of the message component. Animates the initial appearance (opacity: 0, y: -20), transitioning to full visibility (opacity: 1, y: 0) over a duration of 0.5 seconds.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -13,7 +13,14 @@ const Message = ({ email }) => {
         dispatch(setSelectedMail(email));
         navigate(`/mail/${email.id}`);
     }
-    
-    
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            onClick={openMail} className="flex items-start justify-between border-b border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md">
+            
+        </motion.div>
+    )
 }
 
