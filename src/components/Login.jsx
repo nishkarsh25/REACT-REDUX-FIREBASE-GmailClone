9b90ae05dff5cacac7c3a6c98c24e00d84ed2c73
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:41:19 +0530

    Import Statements React: The React import brings in the core React library, which is necessary to create React components. Firebase Authentication Methods: signInWithPopup is imported from firebase/auth to handle the popup sign-in method for Firebase. Firebase Configuration: auth and provider are imported from the ../firebase file. These are configured instances for Firebase Authentication and the Google Auth provider, respectively. GoogleButton: A third-party component for rendering a Google sign-in button is imported from the react-google-button package. Redux Hooks and Actions: useDispatch is imported from react-redux to dispatch actions to the Redux store, and setAuthUser is imported from the ../redux/appSlice file to update the authenticated user’s state.

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../firebase';
+import GoogleButton from 'react-google-button';
+import { useDispatch } from 'react-redux';
+import { setAuthUser } from '../redux/appSlice';
+
+
+
