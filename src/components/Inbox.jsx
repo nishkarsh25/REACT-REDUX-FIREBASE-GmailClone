f5c45477cbb1128b8f7e91914d6ced0bee18542b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:56:33 +0530

    Import Statements React and Hooks: React, useState from react for creating the component and managing local state. Icons: Various icons from react-icons for visual elements in the UI. Components: Messages for displaying the list of emails. Redux: useSelector from react-redux to access the Redux store.

---
+++ src/components/Inbox.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { MdCropSquare } from "react-icons/md";
+import { FaCaretDown,FaUserFriends } from "react-icons/fa";
+import { IoMdRefresh, IoMdMore } from "react-icons/io";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight,MdInbox } from "react-icons/md";
+import { GoTag } from "react-icons/go";
+import Messages from './Messages';
+import { useSelector } from 'react-redux';
+
+
+
+
+
