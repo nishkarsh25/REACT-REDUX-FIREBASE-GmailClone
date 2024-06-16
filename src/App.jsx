8efeb8f418c22c3940e7952584a672517067276f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:43:43 +0530

    Import Statements Component Imports: The Navbar, Inbox, SendEmail, Mail, Body, and Login components are imported from their respective paths. These components make up different parts of the user interface. React Router: createBrowserRouter and RouterProvider from react-router-dom are imported to handle routing within the application. Redux Hooks: useDispatch and useSelector from react-redux are imported to interact with the Redux store. React: The useEffect hook is imported from react to handle side effects. Firebase: The auth object is imported from the Firebase configuration file to manage authentication. Redux Actions: setAuthUser is imported from the appSlice file to update the authenticated user state in the Redux store.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import Inbox from './components/Inbox';
+import SendEmail from './components/SendEmail';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Mail from './components/Mail';
+import Body from './components/Body';
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './components/Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { setAuthUser } from './redux/appSlice';
+
+
+
+
+
+
