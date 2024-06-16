1802018ddbeb7fd2e24810d26bb27196473ff381
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:48:16 +0530

    Import Statements React and State Hook: React and useState are imported from the react library. useState is used to manage the component's local state. React Icons: Various icons from different icon libraries (react-icons) are imported for use in the sidebar. LuPencil: Pencil icon used for the "Compose" button. IoMdStar: Star icon used for the "Starred" sidebar item. MdOutlineWatchLater: Clock icon used for the "Snoozed" sidebar item. MdOutlineKeyboardArrowDown: Down arrow icon used for the "More" option. MdOutlineDrafts: Drafts icon used for the "Drafts" sidebar item. MdInbox: Inbox icon used for the "Inbox" sidebar item. TbSend2: Send icon used for the "Sent" sidebar item. Redux: useDispatch is imported from react-redux to dispatch actions to the Redux store. Redux Action: setOpen is imported from the appSlice file to handle the state change for opening the compose email modal.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { LuPencil } from "react-icons/lu";
+import { IoMdStar } from "react-icons/io";
+import { MdOutlineWatchLater,MdOutlineKeyboardArrowDown,MdOutlineDrafts,MdInbox } from "react-icons/md";
+import { TbSend2 } from "react-icons/tb";
+import { useDispatch } from 'react-redux';
+import { setOpen } from '../redux/appSlice';
+
+
+
+
