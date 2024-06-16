39f41d5298b48a97fe9c086dea02dfa976027889
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:35:36 +0530

    b. initialState The initialState property defines the initial state of the slice. It is an object that includes the following properties:
    open: A boolean that represents whether a certain part of the app is open or not. It is initialized to false.
    selectedMail: Initially null, this property holds the currently selected email.
    searchText: A string used to store the current search text, initialized to an empty string.
    emails: This property is initialized to null but could be an array or object depending on the application's logic. It is intended to hold the list of emails.
    authUser: Initially null, this property holds the authenticated user's information.

--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -2,7 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const appSlice = createSlice({
     name: "app",
-    
+    initialState: {
+        open: false,
+        selectedMail: null,
+        searchText: "",
+        emails: null, // This might need to be initialized as an array or object depending on your application logic
+        authUser: null
+    },
     
 });
 
