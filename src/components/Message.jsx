75515afb61070f806731d5a6e4ca6846f92fe2ad
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 09:01:18 +0530

    Import Statements React Icons: MdCropSquare, RiStarLine from react-icons/md and react-icons/ri respectively are imported for visual elements in the UI. React Hooks: useDispatch from react-redux is imported to dispatch actions to the Redux store. React Router: useNavigate from react-router-dom is imported to navigate programmatically to different routes. Redux Action: setSelectedMail from ../redux/appSlice is imported to update the selected email in the Redux store. Framer Motion: motion from framer-motion is imported to add animations to the component.

---
+++ src/components/Message.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MdCropSquare } from "react-icons/md";
+import { RiStarLine } from "react-icons/ri";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSelectedMail } from '../redux/appSlice';
+import { motion } from 'framer-motion';
+
+
+
