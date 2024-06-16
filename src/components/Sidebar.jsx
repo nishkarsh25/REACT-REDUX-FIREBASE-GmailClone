48e9accfadb9ccd80eced88c83f412724ba45720
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:48:30 +0530

    Sidebar Items An array named sidebarItems is defined. Each item in the array is an object containing: icon: JSX for the icon. text: A string representing the label for the sidebar item.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,28 @@ import { TbSend2 } from "react-icons/tb";
 import { useDispatch } from 'react-redux';
 import { setOpen } from '../redux/appSlice';
 
+const sidebarItems = [
+    {
+        icon: <MdInbox size={"20px"} />,
+        text: "Inbox"
+    },
+    {
+        icon: <IoMdStar size={"20px"} />,
+        text: "Starred"
+    },
+    {
+        icon: <MdOutlineWatchLater size={"20px"} />,
+        text: "Snoozed"
+    },
+    {
+        icon: <TbSend2 size={"20px"} />,
+        text: "Sent"
+    },
+    {
+        icon: <MdOutlineDrafts size={"20px"} />,
+        text: "Drafts"
+    },
+]
 
 
 
