5c6b8f71c4353869c87afffb60f9df348b5f6f6f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 08:59:34 +0530

    Component State State Variables: searchText: Holds the current search text entered by the user to filter emails. emails: Stores the array of emails fetched from Firestore and stored in Redux state. filterEmail: Stores the filtered emails based on searchText. Redux Integration Redux Selector: useSelector hook subscribes to the Redux store and extracts searchText and emails from the app slice of the state.

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -5,5 +5,15 @@ import { db } from '../firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { setEmails } from '../redux/appSlice';
 
+const Messages = () => {
+  const { searchText, emails } = useSelector(store => store.app);
+  const [filterEmail, setFilterEmail] = useState(emails);
+  const dispatch = useDispatch();
 
+  
+
+  
+
+  
+}
 
